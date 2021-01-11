fd61f9854a83323e0bfc1e755ff9355b8246164d
rafeeek <refoo@outlook.com>
2021-01-11 02:24:42 +0200

    S

--- js/main.js
+++ js/main.js
@@ -1,58 +1,58 @@
 new WOW().init();
 let navmenu = $(".navmenu").outerWidth();
 let cat = document.querySelectorAll(".category p")
-$(document).ready(()=>{
+$(document).ready(() => {
     $(".shadow").addClass("hideee")
-    $("body").css("overflow","auto")
+    $("body").css("overflow", "auto")
 })
 
 //---------------------------------------------------------------------nav
-$(".centericon").click(function() { 
+$(".centericon").click(function () {
     let left = $(".contain").css("left")
-    if( left < "0"){
+    if (left < "0") {
         $(".divtoinmate").fadeOut(10)
-        $(".contain").animate({left : 0} , 500)
+        $(".contain").animate({ left: 0 }, 500)
         $(".centericon i").removeClass("fa-bars");
         $(".centericon i").addClass("fa-times");
         showcat()
-    }else{
-        $(".contain").animate({left : -234.5} , 500)
+    } else {
+        $(".contain").animate({ left: -234.5 }, 500)
         let left = $(".contain").css("left")
         $(".centericon i").removeClass("fa-times");
         $(".centericon i").addClass("fa-bars");
-        hidecat() 
+        hidecat()
         $(".divtoinmate").delay(500).fadeIn(0);
-        
+
     }
 });
 
 // for show category in nav 
-function showcat(){
-    $(".category p").eq(0).animate({top:0},500)
-    $(".category p").eq(1).delay(100).animate({top:0},500)
-    $(".category p").eq(2).delay(200).animate({top:0},500)
-    $(".category p").eq(3).delay(300).animate({top:0},500)
-    $(".category p").eq(4).delay(400).animate({top:0},500)
-    $(".category p").eq(5).delay(500).animate({top:0},500)
+function showcat() {
+    $(".category p").eq(0).animate({ top: 0 }, 500)
+    $(".category p").eq(1).delay(100).animate({ top: 0 }, 500)
+    $(".category p").eq(2).delay(200).animate({ top: 0 }, 500)
+    $(".category p").eq(3).delay(300).animate({ top: 0 }, 500)
+    $(".category p").eq(4).delay(400).animate({ top: 0 }, 500)
+    $(".category p").eq(5).delay(500).animate({ top: 0 }, 500)
 }
 
 // for hide category in nav 
 function hidecat() {
-    $(".category p").eq(0).animate({top:290},200)
-    $(".category p").eq(1).animate({top:290},200)
-    $(".category p").eq(2).animate({top:290},200)
-    $(".category p").eq(3).animate({top:290},200)
-    $(".category p").eq(4).animate({top:290},200)
-    $(".category p").eq(5).animate({top:290},200)
+    $(".category p").eq(0).animate({ top: 290 }, 200)
+    $(".category p").eq(1).animate({ top: 290 }, 200)
+    $(".category p").eq(2).animate({ top: 290 }, 200)
+    $(".category p").eq(3).animate({ top: 290 }, 200)
+    $(".category p").eq(4).animate({ top: 290 }, 200)
+    $(".category p").eq(5).animate({ top: 290 }, 200)
 }
 
 // for hover on category in nav 
-cat.forEach(function(ca){
-    ca.addEventListener("mouseover" , ()=>{
+cat.forEach(function (ca) {
+    ca.addEventListener("mouseover", () => {
         $(ca).addClass("border-right")
         $(ca).removeClass("border-left")
     })
-    ca.addEventListener("mouseout" , ()=>{
+    ca.addEventListener("mouseout", () => {
         $(ca).removeClass("border-right")
         $(ca).addClass("border-left")
     })
@@ -69,18 +69,18 @@ cat.forEach(function(ca){
 function search(val) {
     let serval = `https://api.themoviedb.org/3/search/movie?api_key=eba8b9a7199efdcb0ca1f96879b83c44&query=${val}`
     display_items(serval)
-    if(val == ""){
+    if (val == "") {
         changecat("now_playing")
     }
 }
 
 
-function searchword(val){
-    let search = arrayitems.filter((ee)=>{
-        if(ee.original_title.includes(val.toUpperCase()))
-        {return ee}})
-    let lastsearch =search.map(function(arr) {
-    return `<div class="col-md-4 col-sm-6 mb-3 wow bounceIn">
+function searchword(val) {
+    let search = arrayitems.filter((ee) => {
+        if (ee.original_title.includes(val.toUpperCase())) { return ee }
+    })
+    let lastsearch = search.map(function (arr) {
+        return `<div class="col-md-4 col-sm-6 mb-3 wow bounceIn">
     <div  class="dispaly_contain">
     <img src="https://image.tmdb.org/t/p/w500/${arr.poster_path}" class="img-fluid rounded" alt="">
     <div class="detials rounded p-3"></div>
@@ -94,8 +94,8 @@ function searchword(val){
     </div>
     </div>
     </div>`})
-    $(".refoo").html(lastsearch) 
-    }
+    $(".refoo").html(lastsearch)
+}
 
 // ---------------------------------- end search
 
@@ -108,35 +108,35 @@ function searchword(val){
 
 //--------------------display
 document.addEventListener("load", changecat("now_playing"))
-cat.forEach(function(ca) {
-    ca.addEventListener("click",()=>{
-       let target =  ca.dataset.id
-       if(target == "trending"){
-        trending()
-       }else{
-        changecat(target)
-       }
+cat.forEach(function (ca) {
+    ca.addEventListener("click", () => {
+        let target = ca.dataset.id
+        if (target == "trending") {
+            trending()
+        } else {
+            changecat(target)
+        }
     })
 })
 
-function trending(){
+function trending() {
     let trend = `https://api.themoviedb.org/3/trending/all/day?api_key=eba8b9a7199efdcb0ca1f96879b83c44`
     display_items(trend)
-    $("html,body").animate({scrollTop:0},500)
+    $("html,body").animate({ scrollTop: 0 }, 500)
 }
 
-function changecat(e){
-    let gecat = `https://api.themoviedb.org/3/movie/`+e+`?api_key=eba8b9a7199efdcb0ca1f96879b83c44`
+function changecat(e) {
+    let gecat = `https://api.themoviedb.org/3/movie/` + e + `?api_key=eba8b9a7199efdcb0ca1f96879b83c44`
     display_items(gecat)
-    $("html,body").animate({scrollTop:0},500)
+    $("html,body").animate({ scrollTop: 0 }, 500)
 }
 
 // display all items 
 async function display_items(targetcat) {
-    let x = await fetch (targetcat)
+    let x = await fetch(targetcat)
     let array = await x.json()
     globalThis.arrayitems = array.results
-    let datatodis = arrayitems.map(function(arr) {
+    let datatodis = arrayitems.map(function (arr) {
         return `<div class="col-md-4 col-sm-6 mb-5 wow bounceIn">
         <div  class="dispaly_contain">
             <img src="https://image.tmdb.org/t/p/w500/${arr.poster_path}" class="img-fluid rounded" alt="">
@@ -162,26 +162,26 @@ async function display_items(targetcat) {
 
 // ----------------------------------------regex   
 
-function validtext(inputtar,regex) {
-    if(regex.test(inputtar.value)){
+function validtext(inputtar, regex) {
+    if (regex.test(inputtar.value)) {
         $(inputtar).next().addClass("hideee")
         $(".mybtn").removeClass("disabled")
-    }else{
+    } else {
         $(inputtar).next().removeClass("hideee");
         $(".mybtn").addClass("disabled")
     }
 }
-$(".allinputs input").keyup(function(e) { 
-    validtext(e.target,pattern[e.target.attributes.name.value])
+$(".allinputs input").keyup(function (e) {
+    validtext(e.target, pattern[e.target.attributes.name.value])
 });
 
-const pattern= {
-    name:/^[a-z\d]{5,12}$/,
-    password:/^[\w@-]{8,20}$/,
-    rpassword:/^[\w@-]{8,20}$/,
-    phone:/^\d{11}$/,
-    email:/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-    age:/^\S[0-9]{0,3}$/
+const pattern = {
+    name: /^[a-z\d]{5,12}$/,
+    password: /^[\w@-]{8,20}$/,
+    rpassword: /^[\w@-]{8,20}$/,
+    phone: /^\d{11}$/,
+    email: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+    age: /^\S[0-9]{0,3}$/
 }
 
 // // ----------------------------------------end regex   
@@ -189,49 +189,49 @@ const pattern= {
 
 // other
 
-$(window).scroll(()=>{
+$(window).scroll(() => {
     let scrol = $(window).scrollTop()
-    if(scrol > "500" ){
+    if (scrol > "500") {
         $(".divtoinmate").addClass("floting")
-    }else{
+    } else {
         $(".divtoinmate").removeClass("floting")
     }
 })
-$(".divtoinmate").click(()=>{
-    $("html,body").animate({scrollTop:0},1000)
+$(".divtoinmate").click(() => {
+    $("html,body").animate({ scrollTop: 0 }, 1000)
 })
 
 
 
 
 
-$(".Contactus").click(()=>{
+$(".Contactus").click(() => {
     let Contactoffset = $("#contacusoff").offset().top
-    $("html,body").animate({scrollTop:Contactoffset},1000)
+    $("html,body").animate({ scrollTop: Contactoffset }, 1000)
 })
 
 
 first()
 function first() {
-    if (localStorage.getItem("favmovie") == null){
+    if (localStorage.getItem("favmovie") == null) {
         globalThis.Watchlist = []
         bookdisplay()
-    }else{
+    } else {
         globalThis.Watchlist = JSON.parse(localStorage.getItem("favmovie"))
         bookdisplay()
     }
 }
 
-function fav(){
-    $(".savebtn i").click((e)=>{
-        let fav =  $(e.target).data( "id")
+function fav() {
+    $(".savebtn i").click((e) => {
+        let fav = $(e.target).data("id")
         $(e.target).fadeOut(700)
         Watchlist.push(fav)
         lstfilter()
     })
 }
-function lstfilter(){
-    localStorage.setItem("favmovie" , JSON.stringify(Array.from(new Set(Watchlist))))
+function lstfilter() {
+    localStorage.setItem("favmovie", JSON.stringify(Array.from(new Set(Watchlist))))
     first()
 }
 
@@ -240,31 +240,32 @@ function lstfilter(){
 
 // ------------------------------------------------------------------------------------------bookmark
 let bookWidth = $(".book").outerWidth()
-$(".book").css("right" , -bookWidth+13)
-$(".after").click(()=>{
+$(".book").css("right", -bookWidth + 13)
+$(".after").click(() => {
     let bookright = $(".book").css("right")
-    if(bookright <= "0"){
-        $("body").css("overflow","hidden")
-        $(".book").animate({right:0},700)
-          $(".openclose").removeClass("fa-bookmark");
-          $(".openclose").addClass("fa-times-circle");
-    }else{
-        $(".book").animate({right:-bookWidth+14},700)   
+    if (bookright <= "0") {
+        $("body").css("overflow", "hidden")
+        $(".book").animate({ right: 0 }, 700)
+        $(".openclose").removeClass("fa-bookmark");
+        $(".openclose").addClass("fa-times-circle");
+    } else {
+        $(".book").animate({ right: -bookWidth + 14 }, 700)
         $(".openclose").addClass("fa-bookmark");
         $(".openclose").removeClass("fa-times-circle");
-        $("body").css("overflow","auto")
+        $("body").css("overflow", "auto")
     }
 })
 
 async function bookdisplay() {
     globalThis.moviearray = []
     let hasala = ``
-    for(i=0 ; i<Watchlist.length ; i++){
-        let x = await fetch (`https://api.themoviedb.org/3/movie/+${Watchlist[i]}+?api_key=eba8b9a7199efdcb0ca1f96879b83c44`)
+    for (i = 0; i < Watchlist.length; i++) {
+        let x = await fetch(`https://api.themoviedb.org/3/movie/+${Watchlist[i]}+?api_key=eba8b9a7199efdcb0ca1f96879b83c44`)
         moviearray.push(await x.json())
     }
-    for(t=0 ; t<moviearray.length ; t++){
-        hasala +=`<div class="col-md-4 col-sm-6 mb-3">
+
+    for (t = 0; t < moviearray.length; t++) {
+        hasala += `<div class="col-md-4 col-sm-6 mb-3">
         <div  class="dispaly_contain">
             <img src="https://image.tmdb.org/t/p/w500/${moviearray[t].poster_path}" class="img-fluid rounded" alt="">
             <div class="detials rounded p-1"></div>
@@ -285,13 +286,13 @@ async function bookdisplay() {
 
 
 function calldelet() {
-    $(".deletmovie i").click((e)=>{
-        let delet =  $(e.target).data("id")
-        Watchlist.splice(Watchlist.indexOf(delet),1)
+    $(".deletmovie i").click((e) => {
+        let delet = $(e.target).data("id")
+        Watchlist.splice(Watchlist.indexOf(delet), 1)
         lstfilter()
-    })   
+    })
 }
-$(".clearlocal").click(()=>{
+$(".clearlocal").click(() => {
     localStorage.clear()
     first()
 })
